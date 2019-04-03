4f9da4ad4033a1e7f0b04a4bc24572587fde7aee
davidadsit <david.adsit@gmail.com>
2019-04-03 09:42:37 -0600

    create random 2 and 3 operatnd problems

--- coordinator.tests/JobPoolTests.cs
+++ coordinator.tests/JobPoolTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using coordinator.Application;
 using NUnit.Framework;
 
@@ -42,7 +44,49 @@ namespace coordinator.tests
         public void GetNextJob_gets_a_job_of_difficulty_10_for_high_level_calculators()
         {
             var jobPool = new JobPool();
-            Assert.That(jobPool.GetNextJob(70).Difficulty, Is.EqualTo(10));
+            Assert.That(jobPool.GetNextJob(850).Difficulty, Is.EqualTo(10));
+        }
+
+        [Test]
+        [Repeat(1000)]
+        public void GetNextJob_gets_a_generated_simple_problem()
+        {
+            var simpleProblemPattern = @"^CALCULATE: \d{1,4} [-+/*] \d{1,4}$";
+            var simpleSolutionPattern = @"^-?\d+(\.\d{1,3})?$";
+
+            var jobPool = new JobPool();
+            var job = jobPool.GetNextJob(0);
+            Assert.That(job.Difficulty, Is.EqualTo(0));
+            Assert.That(Regex.IsMatch(job.Problem, simpleProblemPattern));
+            Assert.That(Regex.IsMatch(job.Solution, simpleSolutionPattern));
+        }
+
+        [Test]
+        [Repeat(1000)]
+        public void GetNextJob_gets_a_generated_simple_problem_with_negative_numbers()
+        {
+            var negativeNumbersPattern = @"^CALCULATE: -?\d{1,4} [-+/*] -?\d{1,4}$";
+            var simpleSolutionPattern = @"^-?\d+(\.\d{1,3})?$";
+
+            var jobPool = new JobPool();
+            var job = jobPool.GetNextJob(15);
+            Assert.That(job.Difficulty, Is.EqualTo(1));
+            Assert.That(Regex.IsMatch(job.Problem, negativeNumbersPattern));
+            Assert.That(Regex.IsMatch(job.Solution, simpleSolutionPattern));
+        }
+
+        [Test]
+        [Repeat(10000)]
+        public void GetNextJob_gets_a_generated_3_operand_problem()
+        {
+            var threeOperandPattern = @"^CALCULATE: -?\d{1,4} [-+/*] -?\d{1,4} [-+/*] -?\d{1,4}$";
+            var simpleSolutionPattern = @"^-?\d+(\.\d{1,3})?$";
+
+            var jobPool = new JobPool();
+            var job = jobPool.GetNextJob(25);
+            Assert.That(job.Difficulty, Is.EqualTo(2));
+            Assert.That(Regex.IsMatch(job.Problem, threeOperandPattern));
+            Assert.That(Regex.IsMatch(job.Solution, simpleSolutionPattern));
         }
     }
 }
--- coordinator/Application/JobPool.cs
+++ coordinator/Application/JobPool.cs
@@ -15,27 +15,27 @@ namespace coordinator.Application
 
         List<Job> jobs = new List<Job>
         {
-            new Job { Difficulty = 2, Problem = "CALCULATE: (3 + 4) * 5", Solution = "35" },
-            new Job { Difficulty = 2, Problem = "CALCULATE: 5 * (3 + 4)", Solution = "35" },
-            new Job { Difficulty = 2, Problem = "CALCULATE: 3^4", Solution = "81" },
-            new Job { Difficulty = 2, Problem = "CALCULATE: (3 * 4)^5", Solution = "248832" },
-            new Job { Difficulty = 2, Problem = "CALCULATE: 400^.5", Solution = "20" },
+            new Job { Difficulty = 3, Problem = "CALCULATE: (3 + 4) * 5", Solution = "35" },
+            new Job { Difficulty = 3, Problem = "CALCULATE: 5 * (3 + 4)", Solution = "35" },
+            new Job { Difficulty = 3, Problem = "CALCULATE: 3^4", Solution = "81" },
+            new Job { Difficulty = 3, Problem = "CALCULATE: (3 * 4)^5", Solution = "248832" },
+            new Job { Difficulty = 3, Problem = "CALCULATE: 400^.5", Solution = "20" },
 
-            new Job { Difficulty = 3, Problem = "CALCULATE: 26 % 5", Solution = "1" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: 26 modulo 5", Solution = "1" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: 5(6)", Solution = "30" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: 100 log 10", Solution = "2" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: ln e", Solution = "1" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: ln e^3", Solution = "3" },
-            new Job { Difficulty = 3, Problem = "CALCULATE: sqrt 400", Solution = "20" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: 26 % 5", Solution = "1" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: 26 modulo 5", Solution = "1" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: 5(6)", Solution = "30" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: 100 log 10", Solution = "2" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: ln e", Solution = "1" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: ln e^3", Solution = "3" },
+            new Job { Difficulty = 4, Problem = "CALCULATE: sqrt 400", Solution = "20" },
 
-            new Job { Difficulty = 4, Problem = "CALCULATE: IV plus III", Solution = "VII" },
-            new Job { Difficulty = 4, Problem = "CALCULATE: XIV times IX", Solution = "CXXVI" },
+            new Job { Difficulty = 5, Problem = "CALCULATE: IV plus III", Solution = "VII" },
+            new Job { Difficulty = 5, Problem = "CALCULATE: XIV times IX", Solution = "CXXVI" },
 
-            new Job { Difficulty = 5, Problem = "CALCULATE: sqrt -81", Solution = "9i" },
-            new Job { Difficulty = 5, Problem = "CALCULATE: √(-81)", Solution = "9i" },
+            new Job { Difficulty = 6, Problem = "CALCULATE: sqrt -81", Solution = "9i" },
+            new Job { Difficulty = 6, Problem = "CALCULATE: √(-81)", Solution = "9i" },
 
-            new Job { Difficulty = 6, Problem = "CALCULATE: √(x^2 + 4x + 4)", Solution = "x + 2" },
+            new Job { Difficulty = 7, Problem = "CALCULATE: √(x^2 + 4x + 4)", Solution = "x + 2" },
     };
 
         public Job GetNextJob(int previousCorrectResponses)
@@ -47,6 +47,8 @@ namespace coordinator.Application
                     return CreateSimpleJob(difficulty, 0, 1000);
                 case 1:
                     return CreateSimpleJob(difficulty, -1000, 1000);
+                case 2:
+                    return CreateThreeOperandJob();
                 default:
                     return jobs.Where(x => x.Difficulty == difficulty).OrderBy(x => Guid.NewGuid().ToString()).FirstOrDefault() ?? new Job() { Difficulty = 10, Problem = "CALCULATE: The sum of all numbers, less than one million, which are palindromic in base 10 and base 2", Solution = "872187" };
             }
@@ -57,32 +59,172 @@ namespace coordinator.Application
             decimal operand1 = random.Next(operandMin, operandMax);
             decimal operand2 = random.Next(operandMin, operandMax);
             var operation = "";
-            var solution = "";
+            decimal solution = 0;
 
             switch (random.Next(0, 4))
             {
                 case 0:
                     operation = "+";
-                    solution = $"{operand1 + operand2}";
+                    solution = operand1 + operand2;
                     break;
                 case 1:
                     operation = "-";
-                    solution = $"{operand1 - operand2}";
+                    solution = operand1 - operand2;
                     break;
                 case 2:
                     operation = "*";
-                    solution = $"{operand1 * operand2}";
+                    solution = operand1 * operand2;
                     break;
                 case 3:
+                    if (operand2 == 0)
+                    {
+                        operand2 = 1;
+                    }
+
                     operation = "/";
-                    solution = $"{operand1 / operand2:0.###}";
+                    solution = operand1 / operand2;
                     break;
             }
             return new Job
             {
                 Difficulty = level,
                 Problem = $"CALCULATE: {operand1} {operation} {operand2}",
-                Solution = solution
+                Solution = $"{solution:0.###}"
+            };
+        }
+        private Job CreateThreeOperandJob()
+        {
+            decimal operand1 = random.Next(-1000, 1000);
+            decimal operand2 = random.Next(-1000, 1000);
+            decimal operand3 = random.Next(-1000, 1000);
+            var operation1 = "";
+            var operation2 = "";
+            decimal solution = 0;
+
+            switch (random.Next(0, 4))
+            {
+                case 0:
+                    operation1 = "+";
+                    operation2 = "+";
+                    solution = operand1 + operand2 + operand3;
+                    break;
+                case 1:
+                    operation1 = "+";
+                    operation2 = "-";
+                    solution = operand1 + operand2 - operand3;
+                    break;
+                case 2:
+                    operation1 = "+";
+                    operation2 = "*";
+                    solution = operand1 + operand2 * operand3;
+                    break;
+                case 3:
+                    operation1 = "+";
+                    operation2 = "/";
+                    if (operand3 == 0)
+                    {
+                        operand3 = 1;
+                    }
+
+                    solution = operand1 + operand2 / operand3;
+                    break;
+                case 4:
+                    operation1 = "-";
+                    operation2 = "+";
+                    solution = operand1 - operand2 + operand3;
+                    break;
+                case 5:
+                    operation1 = "-";
+                    operation2 = "-";
+                    solution = operand1 - operand2 - operand3;
+                    break;
+                case 6:
+                    operation1 = "-";
+                    operation2 = "*";
+                    solution = operand1 - operand2 * operand3;
+                    break;
+                case 7:
+                    operation1 = "-";
+                    operation2 = "/";
+                    if (operand3 == 0)
+                    {
+                        operand3 = 1;
+                    }
+
+                    solution = operand1 - operand2 / operand3;
+                    break;
+                case 8:
+                    operation1 = "*";
+                    operation2 = "+";
+                    solution = operand1 * operand2 + operand3;
+                    break;
+                case 9:
+                    operation1 = "*";
+                    operation2 = "-";
+                    solution = operand1 * operand2 - operand3;
+                    break;
+                case 10:
+                    operation1 = "*";
+                    operation2 = "*";
+                    solution = operand1 * operand2 * operand3;
+                    break;
+                case 11:
+                    operation1 = "*";
+                    operation2 = "/";
+                    if (operand3 == 0)
+                    {
+                        operand3 = 1;
+                    }
+
+                    solution = operand1 * operand2 / operand3;
+                    break;
+                case 12:
+                    operation1 = "/";
+                    operation2 = "+";
+                    if (operand2 == 0)
+                    {
+                        operand2 = 1;
+                    }
+                    solution = operand1 / operand2 + operand3;
+                    break;
+                case 13:
+                    operation1 = "/";
+                    operation2 = "-";
+                    if (operand2 == 0)
+                    {
+                        operand2 = 1;
+                    }
+                    solution = operand1 / operand2 - operand3;
+                    break;
+                case 14:
+                    operation1 = "/";
+                    operation2 = "*";
+                    if (operand2 == 0)
+                    {
+                        operand2 = 1;
+                    }
+                    solution = operand1 / operand2 * operand3;
+                    break;
+                case 15:
+                    operation1 = "/";
+                    operation2 = "/";
+                    if (operand2 == 0)
+                    {
+                        operand2 = 1;
+                    }
+                    if (operand3 == 0)
+                    {
+                        operand3 = 1;
+                    }
+
+                    solution = operand1 / operand2 / operand3;
+                    break;
+            }
+            return new Job
+            {
+                Difficulty = 2,
+                Problem = $"CALCULATE: {operand1} {operation1} {operand2} {operation2} {operand3}",
+                Solution = $"{solution:0.###}"
             };
         }
     }
