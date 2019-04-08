d81fbb741dafab7bba58372c9388863a00419ff6
Jonathan Allen <jallen@snow.edu>
2019-04-08 08:32:58 -0600

    change IP addresses

--- worker/Models/RegistrationRequest.cs
+++ worker/Models/RegistrationRequest.cs
@@ -4,9 +4,10 @@ namespace worker.Models
 {
     public class RegistrationRequest
     {
+        public const string YourLocalIP = "144.17.24.145";
         public Guid WorkerId { get; set; }
         public string TeamName { get; set; }
-        public string CreateJobEndpoint { get; set; }
-        public string ErrorCheckEndpoint { get; set; }
+        public string CreateJobEndpoint { get; set; } = $"{YourLocalIP}/CreateJob";
+        public string ErrorCheckEndpoint { get; set; } = $"{YourLocalIP}/ErrorCheck";
     }
 }
